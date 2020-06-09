4294b3533593468054b29b68df8247310c7f63e4
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-06-08 22:21:46 -0500

    objetos anidados

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -15,6 +15,19 @@ export class ReactiveComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  public get nombreNoValido() {
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
+  }
+
+  public get apellidoNoValido() {
+    return (
+      this.forma.get('apellido').invalid && this.forma.get('apellido').touched
+    );
+  }
+
+  public get correoNoValido() {
+    return this.forma.get('correo').invalid && this.forma.get('correo').touched;
+  }
   crearFormulario() {
     this.forma = this.formBuilder.group({
       nombre: ['', [Validators.required, Validators.minLength(5)]],
@@ -23,10 +36,18 @@ export class ReactiveComponent implements OnInit {
         '',
         Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2-3}$'),
       ],
+      direccion: this.formBuilder.group({
+        distrito: ['', Validators.required],
+        ciudad: ['', Validators.required],
+      }),
     });
   }
 
   guardar() {
-    console.log(this.forma);
+    if (this.forma.invalid) {
+      return Object.values(this.forma.controls).forEach((control) => {
+        control.markAsTouched();
+      });
+    }
   }
 }

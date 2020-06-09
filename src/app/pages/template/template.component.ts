131ee0cca17ad25459cb9aee6bd9fcd995581d2c
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-06-08 20:43:25 -0500

    armando form html

---
+++ src/app/pages/template/template.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css'],
+})
+export class TemplateComponent implements OnInit {
+  usuario = {
+    nombre: '',
+    apellido: '',
+    correo: '',
+  };
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  guardar(form: NgForm) {
+    console.log(form.value);
+  }
+}
